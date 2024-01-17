b23a2da47991de6c8c23c8f9c15a02d2093a3754
Yodazon <michael.karas96@gmail.com>
2024-01-17 18:01:36 -0500

    commit

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -18,10 +18,30 @@ def determineStatView() -> bool:
 
 
 def statCheck(stat_check):
+    x = ["Activity (Steps, Calories) - 1",
+         "Sleep - 2",
+         "Weight - 3"
+         ]
     if stat_check is True:
         print("Which stats would you like to check out?")
-        print("Calories - 1")
-        print("Sleep - 2")
-        print("Weight - 3")
-        stat_Choice = input("Please input the corresponding number to check the stat")
         
+        for i in len(x):
+            print(x[i])
+
+        stat_Choice = int(input("Please input the corresponding number to check the stat"))
+        check = False
+        while check is False:
+            if stat_Choice == 1:
+                print("Activity it is!")
+                check = True
+                stats_return = True
+            elif stat_Choice == 2:
+                print("Sleep it is! zzz")s
+                check = True
+                stats_return = False
+            elif stat_Choice == 3:
+                print("Weight it is!")
+                check = True
+                stats_return = False
+            else:
+                print('Try again!')
