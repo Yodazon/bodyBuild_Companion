eac817cb00220e4168571c4b30749b0673d5e20c
Yodazon <michael.karas96@gmail.com>
2024-01-21 14:24:24 -0500

    Setting Up Webpages
    Setting up seperate webpages for each option in thee bodybuilding companion app

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder= 'templates')
 
+
 @app.route('/')
 def index():
         return render_template('index.html')
@@ -12,6 +13,22 @@ def about():
 @app.route('/contact')
 def contact():
         return render_template('contact.html')
+
+
+@app.route('/companion')
+def companion():
+        return render_template('companion.html')
+
+@app.route('/stats')
+def stats():
+        return render_template('stats.html')
+
+@app.route('/workout')
+def workout():
+        return render_template('workout.html')
+
+
+
 
 if __name__ == '__main__':
         app.run(debug=True)
---
+++ app/static/script.js
@@ -0,0 +1,4 @@
+function openPage(page) {
+    console.log(page)
+    window.location.href = page;
+}
