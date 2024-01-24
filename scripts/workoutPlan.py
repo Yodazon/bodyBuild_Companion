2f0aaf11c3940652d8ea29312d0b763f84c2066e
Yodazon <michael.karas96@gmail.com>
2024-01-24 16:20:40 -0500

    script complete

--- scripts/workoutPlan.py
+++ scripts/workoutPlan.py
@@ -13,6 +13,9 @@ from clarifai_grpc.grpc.api.status import status_code_pb2
 
 def workoutGen(RAW_TEXT_INPUT, PAT, USER_ID, APP_ID):
 
+    print("generating workout")
+    wokrout_filename = f"{RAW_TEXT_INPUT}.txt"
+
     channel = ClarifaiChannel.get_grpc_channel()
     stub = service_pb2_grpc.V2Stub(channel)
 
@@ -41,15 +44,13 @@ def workoutGen(RAW_TEXT_INPUT, PAT, USER_ID, APP_ID):
         raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
 
     # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
-    results = post_workflow_results_response.results[0]
+    workout_path = "C:\\Coding\\Github\\bodyBuild_Companion\\app\\generatedWorkout\\" + wokrout_filename
 
-    # Each model we have in the workflow will produce one output.
-    for output in results.outputs:
-        model = output.model
+    results = post_workflow_results_response.results[0].outputs[0].data.text.raw
 
-        print("Predicted concepts for the model `%s`" % model.id)
-        for concept in output.data.concepts:
-            print("	%s %.2f" % (concept.name, concept.value))
+    with open(workout_path, 'w') as f:
+        f.write(str(results))
 
-        # Uncomment this line to print the full Response JSON
-        print(results)
+    print("Workout Done!")
+    
+    return(workout_path)
